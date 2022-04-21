3da7713658ac1e01507ca059cca2113030011fd9
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-20 22:32:28 -0300

    chat, config, eventos, profile e js iniciados

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+const mainButton = document.getElementById("main");
+const chatsButton = document.getElementById("chats");
+const eventosButton = document.getElementById("eventos");
+
+function clearButtons() {
+    if(chatsButton.classList.contains("active_button")) {
+        chatsButton.classList.remove("active_button");
+    }
+    if(eventosButton.classList.contains("active_button")) {
+        eventosButton.classList.remove("active_button");
+    }
+    if(mainButton.classList.contains("active_button")) {
+        mainButton.classList.remove("active_button");
+    }
+}
+
+function addColorButtons() {
+    if(window.location.pathname == '/index.html') {
+        mainButton.classList.add("active_button");
+    }
+    if (window.location.pathname == '/chats.html') {
+        chatsButton.classList.add("active_button");
+    }
+    if (window.location.pathname == '/eventos.html') {
+        eventosButton.classList.add("active_button");
+    }
+}
+
+function buttonFooter() {
+    clearButtons();
+    addColorButtons();
+}
+
+buttonFooter();
