4d4d5646d9ab5104cf7db8ef46a9c576e99832ea
joaovfonsecad <joaovfonsecad@gmail.com>
2022-04-22 17:19:43 -0300

    logo temporario adicionado

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,29 @@
-var mainButton = document.getElementById("main");
+var jobberButton = document.getElementById("jobber");
 var chatsButton = document.getElementById("chats");
 var eventosButton = document.getElementById("eventos");
+var mainButton = document.getElementById("main");
 
 function clearButtons() {
+    if(mainButton.classList.contains("active_button")) {
+        mainButton.classList.remove("active_button");
+    }
+    if(jobberButton.classList.contains("active_button")) {
+        jobberButton.classList.remove("active_button");
+    }
     if(chatsButton.classList.contains("active_button")) {
         chatsButton.classList.remove("active_button");
     }
     if(eventosButton.classList.contains("active_button")) {
         eventosButton.classList.remove("active_button");
     }
-    if(mainButton.classList.contains("active_button")) {
-        mainButton.classList.remove("active_button");
-    }
 }
 
 function addColorButtons() {
-    if(window.location.pathname == '/index.html') {
+    if(window.location.pathname == '/main.html') {
         mainButton.classList.add("active_button");
+    }
+    if (window.location.pathname == '/jobber.html') {
+        jobberButton.classList.add("active_button");
     }
     if (window.location.pathname == '/chats.html') {
         chatsButton.classList.add("active_button");
@@ -24,7 +31,10 @@ function addColorButtons() {
     if (window.location.pathname == '/eventos.html') {
         eventosButton.classList.add("active_button");
     }
-    if(window.location.pathname == '/Challenge/index.html') {
+    if(window.location.pathname == '/Challenge/main.html') {
+        mainButton.classList.add("active_button");
+    }
+    if(window.location.pathname == '/Challenge/jobber.html') {
         mainButton.classList.add("active_button");
     }
     if (window.location.pathname == '/Challenge/chats.html') {
